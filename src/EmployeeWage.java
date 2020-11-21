e13c487485a7a57fb81bf25ea24c332d8d1775f9
chetan <zambare.c123@gmail.com>
2020-11-21 16:10:39 +0530

    Create method wageCalculator, Refactor code

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,35 +1,38 @@
 
 public class EmployeeWage {
+	static int isPartTime = 1;
+	static int isFullTime = 2 ;
+	static final int empRateHrs = 20;
+	static final int maxDay    = 20;
+	static int empHrs =0 ;
+	static int EmpTotalwage=0;
+	static int EmpTotalHrs = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee");
+		wageCalculator();
 		
-		int isPartTime = 1;
-		int isFullTime = 2 ;
-		int empRateHrs = 20;
-		int maxDay    = 20;
-		int empHrs;
-		int EmpTotalwage=0;
-		int EmpTotalHrs = 0;
-		//Employee can be parttime or fulltime on alternate days
-		
+	}
+	
+	
+	public static void wageCalculator() {
 		for (int day =1; day <= maxDay; day++) {
-		int empCheck = (int)Math.floor(Math.random()*10)%3;
-		if ( empCheck == isFullTime){
-			empHrs = 8;
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			if ( empCheck == isFullTime){
+				empHrs = 8;
 			}
-		else if (empCheck == isPartTime){
-			empHrs = 4;
+			else if (empCheck == isPartTime){
+				empHrs = 4;
 			}
-		else {
-			empHrs =0;
-		}
-		EmpTotalHrs += empHrs; 
-		System.out.println("Day# : " + day + " " + "Emp Hrs: " + empHrs );
-		if (EmpTotalHrs > 100) {
-		EmpTotalHrs = 100;
-			break;
-		}
+			else {
+				empHrs =0;
+			}
+			EmpTotalHrs += empHrs; 
+			System.out.println("Day# : " + day + " " + "Emp Hrs: " + empHrs );
+			if (EmpTotalHrs > 100) {
+			EmpTotalHrs = 100;
+				break;
+			}
 		}
 		EmpTotalwage = EmpTotalHrs*empRateHrs;
 		System.out.println("Total Emp Wage :" + EmpTotalwage);
