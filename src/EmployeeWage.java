4ab5037228d5e5a884181217170c4ad65b8d2533
chetan <zambare.c123@gmail.com>
2020-11-21 11:56:28 +0530

    Switch case usePart time Employee added and Employee Wage calculated

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,27 +2,32 @@
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-System.out.println("Welcome Employee");
+		System.out.println("Welcome Employee");
 		
 		int isPartTime = 1;
 		int isFullTime = 2 ;
 		int empCheck = (int)Math.floor(Math.random()*10)%3;
 		int empRateHrs = 20;
-		int empHrs;
+		int empHrs = 0;
 		int empWage;
-		//System.out.println(empCheck);
 		
-		if ( empCheck == isFullTime){
-			empHrs = 8;
-			}
-		else if (empCheck == isPartTime){
+		switch (empCheck) {
+		case (1) :{
 			empHrs = 4;
-			}
-		else {
-			empHrs =0;
+			break;
+		}
+		case (2) :{
+			empHrs = 8;
+			break;
+		}
+		case (3) :{
+			empHrs = 0;
+		}
+			
 		}
 		empWage = empHrs*empRateHrs;
 		System.out.println("Emp Wage : " + empWage);
+
 	}
 
 }
