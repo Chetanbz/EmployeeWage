b1ac7dcd1e711a858eebd0e047d8a11ae22f3c7b
chetan <zambare.c123@gmail.com>
2020-11-21 11:50:15 +0530

    Part time Employee added and Employee Wage calculated

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,23 +2,27 @@
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-        System.out.println("Welcome Employee");
+System.out.println("Welcome Employee");
 		
-		int isFullTime = 1;
-		int empCheck = (int)Math.floor(Math.random()*10)%2;
+		int isPartTime = 1;
+		int isFullTime = 2 ;
+		int empCheck = (int)Math.floor(Math.random()*10)%3;
 		int empRateHrs = 20;
 		int empHrs;
 		int empWage;
+		//System.out.println(empCheck);
 		
 		if ( empCheck == isFullTime){
 			empHrs = 8;
 			}
-			else{
-			empHrs = 0;
+		else if (empCheck == isPartTime){
+			empHrs = 4;
 			}
+		else {
+			empHrs =0;
+		}
 		empWage = empHrs*empRateHrs;
-		System.out.println("Emp Wage: " + empWage);
-
+		System.out.println("Emp Wage : " + empWage);
 	}
 
 }
