6e5454c1ddd811c841a118c2a2b96ae6d82c2958
chetan <zambare.c123@gmail.com>
2020-11-21 16:27:07 +0530

    Modify Wage policy as per Company

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,22 +1,36 @@
+import java.util.Scanner;
 
 public class EmployeeWage {
 	static int isPartTime = 1;
 	static int isFullTime = 2 ;
-	static final int empRateHrs = 20;
-	static final int maxDay    = 20;
-	static int empHrs =0 ;
+	static int empRateHrs = 20;   // limit is set reference purpose
+	static int maxDay    = 31;    // limit is set for reference purpose
+	static int maxHrsMonth = 100; // limit is set for reffference purpose
 	static int EmpTotalwage=0;
 	static int EmpTotalHrs = 0;
 
 	public static void main(String[] args) {
+		System.out.println("provide Company policy on wage");
+		companyCriteria();
 		System.out.println("Welcome Employee");
 		wageCalculator();
+		
+	}
+	public static void companyCriteria() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please provide maximum working Hours in month");
+		maxHrsMonth  = sc.nextInt();
+		System.out.println("Please provide maximum working Days in month");
+		maxDay      = sc.nextInt();
+		System.out.println("Please provide Rate per Hours");
+		empRateHrs = sc.nextInt();
 		
 	}
 	
 	
 	public static void wageCalculator() {
 		for (int day =1; day <= maxDay; day++) {
+			int empHrs = 0;
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
 			if ( empCheck == isFullTime){
 				empHrs = 8;
@@ -29,8 +43,8 @@ public class EmployeeWage {
 			}
 			EmpTotalHrs += empHrs; 
 			System.out.println("Day# : " + day + " " + "Emp Hrs: " + empHrs );
-			if (EmpTotalHrs > 100) {
-			EmpTotalHrs = 100;
+			if (EmpTotalHrs > maxHrsMonth) {
+			EmpTotalHrs = maxHrsMonth;
 				break;
 			}
 		}
