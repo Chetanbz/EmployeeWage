c20f36f70e9618d6c97ed8b472f6261de511fbea
chetan <zambare.c123@gmail.com>
2020-11-21 11:29:48 +0530

    Employee Present or Absent Check

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,6 +3,16 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome Employee");
+		int isFullTime = 1;
+		int empCheck = (int)Math.floor(Math.random()*10)%2;
+		//System.out.println(empCheck);
+		
+		if ( empCheck == isFullTime){
+			System.out.println("Employee is present");
+			}
+			else{
+			System.out.println("Employee is absent");
+			}
 
 	}
 
