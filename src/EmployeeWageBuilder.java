6ebd709ed11f03a569856fcdd045857fe7eef0c8
chetan <zambare.c123@gmail.com>
2020-11-27 23:31:42 +0530

    Search company wage by name

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -13,11 +13,17 @@ public class EmployeeWageBuilder {
 		 Map<String,List> daily = new HashMap<String,List>();
 		while(true) {
 			System.out.println("Do you wish to add Wage for Employee for Company ");
-			System.out.println("Press 1 Add new EmployeeWage for new Company  \n Press 0 Exit ");
+			System.out.println("Press 1 Add new EmployeeWage for new Company \n Press 2 Get EmployeeWage of Company \n Press 0 Exit ");
 			Scanner sc = new Scanner(System.in);
 			int option = sc.nextInt();
 			if (option == 0) {
 				break;
+			}
+			else if(option ==2){
+				System.out.println(" Provide name of company whose wage wants to get");
+				String name = sc.next();
+				System.out.println(empWageCompany.get(name));
+			
 			}
 			System.out.println(" Provide name of company");
 			String name = sc.next();
@@ -27,7 +33,9 @@ public class EmployeeWageBuilder {
 			daily.put(name,empWage.dailyWageCalculator());
 			
 		}
+		System.out.println("Toatal EmployeeWage ");
 		System.out.println(empWageCompany);
+		System.out.println("Daily EmployeeWage of company  ");
 		System.out.println(daily);
 
 	}
